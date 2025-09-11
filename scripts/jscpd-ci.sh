68cf861acef22c50b5ea18144b8da31a17bc3607
lucas-f-v <lucas.vicenzotto@zup.io>
2025-09-11 13:35:49 -0700

    Use merge-based jscpd scripts in CI

--- scripts/jscpd-ci.sh
+++ scripts/jscpd-ci.sh
@@ -2,8 +2,8 @@
 set -eu
 
 # EXIT_CODE=0
-# ./scripts/run-jscpd.sh || EXIT_CODE=$?
-./scripts/run-jscpd.sh || true
-./scripts/post-jscpd-comment.sh || true
+# ./scripts/run-jscpd-merge.sh || EXIT_CODE=$?
+./scripts/run-jscpd-merge.sh || true
+./scripts/post-jscpd-merge-comment.sh || true
 rm -rf jscpd-report jscpd-base.json jscpd-merged.json
 # exit $EXIT_CODE
